2240ef3710b2fb0823f9d234c9bfb92529aa284f
jimreader <github@jimreader.co.uk>
2018-02-18 20:38:27 +0000

    Added initial files

---
+++ src/main/java/uk/co/jimreader/ait/AlexaIntentTesterSpeechlet.java
@@ -0,0 +1,80 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package uk.co.jimreader.ait;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.speechlet.SpeechletV2;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+
+/**
+ * This simple sample has no external dependencies or session management, and
+ * shows the most basic example of how to handle Alexa Skill requests.
+ */
+public class AlexaIntentTesterSpeechlet implements SpeechletV2 {
+	private static final Logger log = LoggerFactory.getLogger(AlexaIntentTesterSpeechlet.class);
+
+	@Override
+	public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
+		log.info("onSessionStarted requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(),
+				requestEnvelope.getSession().getSessionId());
+		// any initialization logic goes here
+	}
+
+	@Override
+	public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> requestEnvelope) {
+		log.info("onLaunch requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(),
+				requestEnvelope.getSession().getSessionId());
+		PlainTextOutputSpeech speech = getPlainTextOutputSpeech("Your intent was Loaded.");
+		return SpeechletResponse.newTellResponse(speech);
+	}
+
+	@Override
+	public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
+		IntentRequest request = requestEnvelope.getRequest();
+		log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+				requestEnvelope.getSession().getSessionId());
+
+		Intent intent = request.getIntent();
+		String intentName = (intent != null) ? intent.getName() : null;
+		PlainTextOutputSpeech speech = getPlainTextOutputSpeech("Your intent was " + intentName);
+		return SpeechletResponse.newTellResponse(speech);
+	}
+
+	@Override
+	public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> requestEnvelope) {
+		log.info("onSessionEnded requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(),
+				requestEnvelope.getSession().getSessionId());
+		// any cleanup logic goes here
+	}
+
+	/**
+	 * Helper method for retrieving an OutputSpeech object when given a string of
+	 * TTS.
+	 * 
+	 * @param speechText
+	 *            the text that should be spoken out to the user.
+	 * @return an instance of SpeechOutput.
+	 */
+	private PlainTextOutputSpeech getPlainTextOutputSpeech(String speechText) {
+		PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText(speechText);
+		return speech;
+	}
+
+}
